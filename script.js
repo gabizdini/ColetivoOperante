927b25a79f4c585a66a4e819be06c801f35febbd
Gabrieli Diniz <128835174+gabizdini@users.noreply.github.com>
2024-11-05 11:41:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,51 @@ function loadNavbar() {
 
 // Carrega a barra de navegação ao iniciar a página
 document.addEventListener("DOMContentLoaded", loadNavbar);
+
+
+// Função para carregar e exibir dados da planilha do Google Sheets
+function loadGoogleSheetData() {
+    // ID da planilha do Google Sheets
+    const spreadsheetId = '1t1KVC8akGxbfKro3eFTMGddUFWP--Yj5vaerVdVuBFE';
+    // ID da planilha dentro do documento (geralmente 0 para a primeira planilha)
+    const sheetId = 0;
+
+    gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: spreadsheetId,
+        range: 'evento!A2:Z' // Substitua pelo nome da aba que você deseja ler
+    }).then(function(response) {
+        const data = response.result.values;
+        const tableBody = document.querySelector('#actions-table tbody');
+
+        // Limpe qualquer conteúdo existente na tabela
+        tableBody.innerHTML = '';
+
+        // Preencha a tabela com os dados da planilha
+        data.forEach(function(row) {
+            const rowData = row.map(item => item || ''); // Lida com valores nulos ou indefinidos
+
+            const tableRow = document.createElement('tr');
+            rowData.forEach(function(cellData) {
+                const cell = document.createElement('td');
+                cell.textContent = cellData;
+                tableRow.appendChild(cell);
+            });
+
+            tableBody.appendChild(tableRow);
+        });
+    });
+}
+
+// Função para inicializar a API do Google Sheets
+function initGoogleSheetsApi() {
+    gapi.client.init({
+        apiKey: 'AIzaSyBXzRa51UzuTHkJXUmyxhcmO8Owq4CiTVs',
+        discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    }).then(function() {
+        loadGoogleSheetData();
+    });
+}
+
+// Carrega a API do Google Sheets e inicia a aplicação
+gapi.load('client', initGoogleSheetsApi);
+
